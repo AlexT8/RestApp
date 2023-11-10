e0448a1c586977840caf980370ae728ed64491c9
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-10 19:29:17 -0300

    created constants for colors

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+export const constants = {
+    colors: {
+        background: '#F9F9FB',
+        text: '#313234',
+        primary: '#F5CA48',
+        secondary: '#F26C68',
+        lightText: '#CDCDCD',
+        price: '#E4723C'
+    }
+}
