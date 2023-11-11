4723c5bf737a3555aace839c5b96c27f0dbd9487
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 13:38:52 -0300

    fonts interface

--- components/general/StyledText.tsx
+++ components/general/StyledText.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components/native"
 import { constants } from "../../utils/constants"
+import { TextProps } from "react-native";
 
-const StyledText = styled.Text`
+interface StyledTextProps extends TextProps {
+    font?: string;
+    size?: string;
+}
+
+const StyledText = styled.Text<StyledTextProps>`
     color: ${constants.colors.text};
     font-family: ${(props: any)=>props.font || 'M400'};
     font-size: ${(props: any)=>props.size || 16};
