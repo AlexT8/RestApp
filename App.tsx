38a6ad1a71c2c4adf8ae817ce9106a2165188fab
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 12:36:06 -0300

    removed unused component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-import styled from 'styled-components/native';
-import { constants } from './utils/constants';
 import HomeScreen from './screens/Home';
-
-const StyledText = styled.Text`
-  font-size: 20px;
-  margin-top: 40px;
-  color: ${constants.colors.secondary};
-`;
 
 export default function App() {
   return (
