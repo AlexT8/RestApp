0842b68ae9fee0dbb75d42216e22479f4d028b5e
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 11:30:34 -0300

    created padding horizontal container

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScreenContainer } from "../styles/general/ScreenContainer"
+import { PaddingContainer, ScreenContainer } from "../styles/general/ScreenContainer"
 import { HomeHeader } from "../components/Home/Header"
 import StyledText from "../components/general/StyledText"
 import TitlesSection from "../components/Home/TitlesSection"
@@ -22,20 +22,24 @@ const HomeScreen = () => {
 
     return(
         <ScreenContainer>
-            <HomeHeader/>
+            <PaddingContainer>
+                <HomeHeader/>
+            </PaddingContainer>
+
             <ScrollView showsVerticalScrollIndicator={false}>
+                <PaddingContainer>
+                    <TitlesSection>
+                        <StyledText style={{fontSize: 16, fontWeight: '400'}}>Food</StyledText>
+                        <StyledText style={{fontSize: 32, fontWeight: "700"}}>Delivery</StyledText>
+                    </TitlesSection>
 
-                <TitlesSection>
-                    <StyledText style={{fontSize: 16, fontWeight: '400'}}>Food</StyledText>
-                    <StyledText style={{fontSize: 32, fontWeight: "700"}}>Delivery</StyledText>
-                </TitlesSection>
+                    <SearchContainer>
+                        <SvgUri source={require('../assets/images/search_icon.svg')} width="20" height="20"/>
+                        <SearchInput placeholder="Search..."/>
+                    </SearchContainer>
 
-                <SearchContainer>
-                    <SvgUri source={require('../assets/images/search_icon.svg')} width="20" height="20"/>
-                    <SearchInput placeholder="Search..."/>
-                </SearchContainer>
-
-                <StyledText style={{fontSize: 16, fontWeight: "700"}}>Categories</StyledText>
+                    <StyledText style={{fontSize: 16, fontWeight: "700"}}>Categories</StyledText>
+                </PaddingContainer>
 
                 <View>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
@@ -46,13 +50,15 @@ const HomeScreen = () => {
                         }
                     </ScrollView>
                 </View>
-
-                <StyledText style={{fontSize: 16, fontWeight: "700", marginTop: 16}}>Popular</StyledText>
                 
-                <FoodCardComponent />
-                <FoodCardComponent />
-                <FoodCardComponent />
-                <FoodCardComponent />
+                <PaddingContainer>
+                    <StyledText style={{fontSize: 16, fontWeight: "700", marginTop: 16}}>Popular</StyledText>
+                    
+                    <FoodCardComponent />
+                    <FoodCardComponent />
+                    <FoodCardComponent />
+                    <FoodCardComponent />
+                </PaddingContainer>
 
             </ScrollView>
         </ScreenContainer>
--- styles/general/ScreenContainer.tsx
+++ styles/general/ScreenContainer.tsx
@@ -6,6 +6,10 @@ export const ScreenContainer = styled.View`
     flex: 1;
     padding-top: ${Constants.statusBarHeight+20};
     background: ${constants.colors.background};
-    padding-left: 18;
-    padding-right: 18;
 `;
+
+export const PaddingContainer = styled.View`
+    width: 100%;
+    padding-left: 20px;
+    padding-right: 20px;
+`
