f1f273442257f9a6f5a28c90aff6c1e47fca5fe3
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 10:28:48 -0300

    added popular title

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -44,6 +44,9 @@ const HomeScreen = () => {
                     }
                 </ScrollView>
             </View>
+
+            <StyledText style={{fontSize: 16, fontWeight: "700", marginTop: 16}}>Popular</StyledText>
+
         </ScreenContainer>
     )
 }
