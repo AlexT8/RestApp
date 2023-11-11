fc3d86d5c3da261d7ce19081c7e916f358c12d02
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 01:02:00 -0300

    added section texts

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,18 @@
 import { ScreenContainer } from "../styles/general/ScreenContainer"
 import { HomeHeader } from "../components/Home/Header"
+import StyledText from "../components/general/StyledText"
+import TitlesSection from "../components/Home/TitlesSection"
 
 const HomeScreen = () => {
     return(
         <ScreenContainer>
             <HomeHeader />
+
+            <TitlesSection>
+                <StyledText style={{fontSize: 16, fontWeight: '400'}}>Food</StyledText>
+                <StyledText style={{fontSize: 32, fontWeight: "700"}}>Delivery</StyledText>
+            </TitlesSection>
+
         </ScreenContainer>
     )
 }
