d1392d01a31e1855f10d292db060853ea6760e5b
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 01:47:18 -0300

    food card created

---
+++ components/Home/FoodCard.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+import { constants } from "../../utils/constants";
+import { TouchableOpacityProps } from "react-native";
+
+interface FoodCardProps extends TouchableOpacityProps {
+    selected?: boolean;
+}
+
+const FoodCard = styled.TouchableOpacity<FoodCardProps>`
+    background: ${(props) =>
+        props.selected ? constants.colors.secondary : "#FFFF"};
+    height: 180;
+    width: 120;
+    border-radius: 20px;
+    margin: 10px;
+    elevation: 4;
+    shadow-color: #000;
+    shadow-offset: 0px 2px;
+    shadow-opacity: 0.3;
+    shadow-radius: 4px;
+`
+
+export default FoodCard
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -5,8 +5,13 @@ import TitlesSection from "../components/Home/TitlesSection"
 import SearchInput from "../components/Home/SearchInput"
 import SearchContainer from "../components/Home/SearchContainer"
 import SvgUri from "react-native-svg-uri"
+import FoodCard from "../components/Home/FoodCard"
+import { Dimensions, ScrollView } from "react-native"
 
 const HomeScreen = () => {
+
+    const screenWidth = Dimensions.get('screen').width;
+
     return(
         <ScreenContainer>
             <HomeHeader />
@@ -22,6 +27,14 @@ const HomeScreen = () => {
             </SearchContainer>
 
             <StyledText style={{fontSize: 16, fontWeight: "700"}}>Categories</StyledText>
+
+            <ScrollView horizontal={true} style={{width: screenWidth, marginLeft: -18, paddingLeft: 10}}>
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+
+            </ScrollView>
         </ScreenContainer>
     )
 }
