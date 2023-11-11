703296142789e8a5007e47e2ee682acaef6a46b8
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-11 13:38:23 -0300

    dynamic font size

--- components/general/StyledText.tsx
+++ components/general/StyledText.tsx
@@ -4,6 +4,7 @@ import { constants } from "../../utils/constants"
 const StyledText = styled.Text`
     color: ${constants.colors.text};
     font-family: ${(props: any)=>props.font || 'M400'};
+    font-size: ${(props: any)=>props.size || 16};
 `
 
 export default StyledText
