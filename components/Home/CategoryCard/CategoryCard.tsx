180d2271e6006cf6ce94fb18256964f3152ceb40
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-15 11:32:26 -0300

    change color text when necessary

--- components/Home/CategoryCard/CategoryCard.tsx
+++ components/Home/CategoryCard/CategoryCard.tsx
@@ -6,16 +6,20 @@ import {
   CategoryCardButton,
   CategoryCardContainer,
 } from './CategoryCard.styles'
+import { useTheme } from 'styled-components'
 
 const icon = require('../../../assets/images/right_arrow.svg')
 const selectedIcon = require('../../../assets/images/right_arrow_selected.svg')
 
 const CategoryCard = ({ card, onPress }: CategoryCardComponentProps) => {
+
+  const theme = useTheme()
+
   return (
     <CategoryCardContainer onPress={onPress} selected={card.selected}>
       <Image source={card.image} style={{ height: 60, width: 60 }} />
 
-      <StyledText size="14px" font="M600">
+      <StyledText size="14px" font="M600" style={{color: card.selected ? theme.white : theme.text}}>
         {card.title}
       </StyledText>
 
