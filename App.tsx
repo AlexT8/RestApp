4b8af252f9b178a788e7ffb39618e9acbca9d353
Alex Taboada <alexandrotaboada@gmail.com>
2023-11-15 11:19:04 -0300

    created themes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import HomeScreen from './screens/Home'
+import {ThemeProvider} from "styled-components";
+import { darkTheme, lightTheme } from './styles/themes';
 
 export default function App() {
-  return <HomeScreen />
+  return (
+    <ThemeProvider theme={false ? lightTheme : darkTheme}>
+      <HomeScreen />
+    </ThemeProvider>
+  )
 }
---
+++ styles/themes.ts
@@ -0,0 +1,5 @@
+import { constants } from "../utils/constants";
+
+export const lightTheme = constants.colors
+
+export const darkTheme = constants.darkColors
